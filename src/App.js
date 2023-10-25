66d436014c5f3e14bd0a6b5593dae6f2f77dc609
Markayel <mtorelus@gmail.com>
2023-10-25 14:31:30 -0700

    added button and words

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Square from "./components/Square";
+import handleRestart from "./components/HandleRestart";
 import "./App.css";
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [player, setPlayer] = useState(0);
-  const [winner, setWinner] = useState();
-  
+  const [player1Wins, setPlayer1Wins] = useState(false);
+  const [player2Wins, setPlayer2Wins] = useState(false);
+  const [noOneWins, setnoOneWins] = useState(false);
   const winConditions = () => {
     const win = [
       [0, 1, 2],
@@ -22,17 +24,22 @@ const App = () => {
       let line = win[i]
       if(squares[line[0]] === "❌" && squares[line[1]] === "❌" && squares[line[2]] === "❌") {
         setTimeout(() => {
-          alert("Player 1 wins!")
+          setPlayer1Wins(true)
         }, 1);
       } else if(squares[line[0]] === "⭕️" && squares[line[1]] === "⭕️" && squares[line[2]] === "⭕️"){
         setTimeout(() => {
-          alert("Player 2 wins!")
+          setPlayer2Wins(true)
         }, 1);
-      }
+      } else if(squares[null] ===  -1)
+      {setnoOneWins(true)
+    }
     }
 
   }
   const mark = (index) => {
+    if (player1Wins || player2Wins || noOneWins)
+    return
+    let updatedGrid = [...squares]
     if (player === 0 && squares[index] === null) {
       let updatedGrid = [...squares];
       updatedGrid[index] = "❌";
@@ -43,7 +50,7 @@ const App = () => {
       updatedGrid[index] = "⭕️";
       setSquares(updatedGrid);
       setPlayer(0);
-    }
+    } 
     winConditions();
   };
 
@@ -55,7 +62,27 @@ const App = () => {
         {squares.map((value, index) => {
           return <Square value={value} key={index} index={index} mark={mark}/>;
         })}
+
+        {player1Wins && ( <div className="player1">
+          Player 1 wins!
+          </div>
+        )
+        }
+
+        {player2Wins && ( <div className="player2">
+          Player 2 wins!
+          </div>
+        )
+        }
+
+        {noOneWins && ( <div className="noOne">
+          You both lose!
+          </div>
+        )
+        }
+
       </div>
+      <ResetButton handleRestart={handleRestart} />
     </>
   );
 };
---
+++ src/components/HandleRestart.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const handleRestart = ({handleReset}) => {
+   
+    return(
+      <>  
+      
+     <div className="reset-button">
+    <button onClick={handleReset}> Play again </button>
+    
+    </div>
+    
+    
+    </>
+
+    )
+}
+export default handleRestart

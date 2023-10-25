f47f67b743ecf7a538646a4c8a09a03bc5663ea2
TheLavitz <lavitz@gmail.com>
2023-10-25 10:37:13 -0700

    added logic to switch players

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
-import React, { useState } from 'react'
-import Square from './components/Square'
-import './App.css'
+import React, { useState } from "react";
+import Square from "./components/Square";
+import "./App.css";
 
 const App = () => {
-   
-   const [squares, setSquares] = useState(Array(9).fill(null))
-  
-   const mark = (index) => {
-    let updatedGrid = [...squares]
-    updatedGrid[index] = "❌"
-    setSquares(updatedGrid)
-    
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [player, setPlayer] = useState(0);
 
-   }
-  return(
-  
-   <>
+  const mark = (index) => {
+    if (player === 0) {
+      let updatedGrid = [...squares];
+      updatedGrid[index] = "❌";
+      setSquares(updatedGrid);
+      setPlayer(1);
+    } else if (player === 1) {
+      let updatedGrid = [...squares];
+      updatedGrid[index] = "⭕️";
+      setSquares(updatedGrid);
+      setPlayer(0);
+    }
+  };
+  return (
+    <>
       <h1>Tic Tac Toe</h1>
-      <div className='grid'>{squares.map ((value, index) => {
-        return(
-          <Square 
-          value={value}
-          key={index}
-          index={index}
-          mark={mark}
-          />
+      <div className="grid">
+        {squares.map((value, index) => {
+          return <Square value={value} key={index} index={index} mark={mark} />;
+        })}
+      </div>
+    </>
+  );
+};
 
-
-        )
-      }
-   
-  )
-}</div>
- </>
-  )
-}
- 
-export default App
+export default App;
